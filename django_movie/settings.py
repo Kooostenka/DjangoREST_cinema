6693b3ec2e163f461dfaa1b54e8d5647ae671b23
Kooostenka <kostyakw@mail.ru>
2021-05-09 19:11:05 +0500

    Docker

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '+(-ne6m64#@6=fh#^lx(kmxnek8d812s(&ajj-y2nf)7cr8##y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,17 +80,27 @@ WSGI_APPLICATION = 'django_movie.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'movie',
+#         'USER': 'postgres',
+#         'PASSWORD': '1',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'movie',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '1',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'PASSWORD': '123456',
+        'HOST': 'dm_db',
+        'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -136,8 +146,10 @@ STATICFILES_DIRS = [STATIC_DIR]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
+    "http://localhost:8081",
     "http://127.0.0.1:8000"
 ]
 
